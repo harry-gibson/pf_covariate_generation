2f5da0b0590772cb59c3a75844da6e57cc1b77e0
harry-gibson <harry.s.gibson@gmail.com>
2018-02-20 10:09:34 +0000

    moving to more coherent mastergrids layout

--- variable_generation/term.py
+++ variable_generation/term.py
@@ -2,6 +2,9 @@ import os, glob
 from datetime import date
 from dateutil.relativedelta import relativedelta
 
+from raster_utilities.utils.geotransform_calcs import CalculatePixelLims
+from raster_utilities.io.tiff_management import GetRasterProperties, ReadAOI_PixelLims
+
 ONEMONTH = relativedelta(months=1)
 
 class SpatialSummaryTypes:
@@ -25,9 +28,16 @@ class TemporalAnomalyTypes:
     DIFF_SYNOPTIC_MONTHLY = "Difference from synoptic monthly"
     NONE = "None"
 
-class FiveKCovariateVariableTerm:
 
-    def __init_(self, Name, DynamicFilenamePattern, SynopticFilenamePattern,
+class FiveKCovariateVariableTerm:
+    # Pass a folder "MasterFolder" which should contain Annual, Monthly, Synoptic folders, or as necessary for the
+    # chosen SpatialSummaryType and TemporalSummaryType.
+
+    # Monthly folder has files with names like {VariableName}.{Year}.{Month}.{TemporalSummary}.{Res}.{SpatialSummary}.tif
+    # Annual folder has files with names like {VariableName}.{Year}.Annual.{TemporalSummary}.{Res}.{SpatialSummary}.tif
+    # Synoptic folder has files with names like {VariableName}.Synoptic.{Month / Overall}.{Res}.{SpatialSummary}.tif
+
+    def __init__(self, Name, MasterFolder,
                 SpatialSummaryType, TemporalSummaryType, TemporalAnomalyType,
                 TemporalLagMonths = 0):
         assert isinstance(SpatialSummaryType, SpatialSummaryTypes)
@@ -35,8 +45,9 @@ class FiveKCovariateVariableTerm:
         assert isinstance(TemporalAnomalyType, TemporalAnomalyTypes)
         assert TemporalLagMonths >= 0 and TemporalLagMonths <= 12
         self.Name = Name
-        self._DynamicPattern = DynamicFilenamePattern
-        self._SynopticPattern = SynopticFilenamePattern
+        self._MasterFolder = MasterFolder
+        #self._DynamicPattern = DynamicFilenamePattern
+        #self._SynopticPattern = SynopticFilenamePattern
         self._SpatialSummaryType = SpatialSummaryType
         self._TemporalSummaryType = TemporalSummaryType
         self._TemporalAnomalyType = TemporalAnomalyType
@@ -45,7 +56,12 @@ class FiveKCovariateVariableTerm:
 
     def _InitialiseFiles(self):
         '''attempt to build dictionaries mapping available dates, years, or months to filenames'''
-        allDynamicFiles = glob.glob(self._DynamicPattern)
+
+        if self._TemporalSummaryType == TemporalSummaryTypes.D_MONTHLY:
+            monthlyFolder = os.path.join(self._MasterFolder, 'Monthly')
+            monthlyPattern = os.path.join(monthlyFolder, '*.{0}.tif'.format(self._SpatialSummaryType))
+            allDynamicFiles = glob.glob(monthlyPattern)
+
         allSynopticFiles = glob.glob(self._SynopticPattern)
         dynamicMonthlyDict = {}
         synopticMthDict = {}
@@ -97,20 +113,56 @@ class FiveKCovariateVariableTerm:
             return str(mth).zfill(2)
 
 
-    def ReadDataForDate(self, RequiredMonth):
+    def ReadDataForDate(self, RequiredDate):
         '''produces an array of data representing the content of the required term for this date
 
-        This takes into account the temporal summary, anomaly, and lag as appropriate for this term'''
-        if self._TemporalSummaryType == TemporalSummaryTypes.D_MONTHLY:
-            dynamicFilename = self.TryGetMonthlyFileForDate(RequiredMonth)
+        This takes into account the temporal summary, anomaly, and lag as appropriate for this term
 
-    def TryGetMonthlyFileForDate(self, RequiredMonth):
-        UseMonth = RequiredMonth - self._TemporalLagMonths * ONEMONTH
-        if self._AllFilesDict.has_key(UseMonth):
-            return self._AllFilesDict[UseMonth]
+        TODO - specify a subset bounding box'''
+        rasterFilename = None
+        if self._TemporalSummaryType == TemporalSummaryTypes.D_MONTHLY:
+            rasterFilename = self.TryGetMonthlyFileForDate(RequiredDate)
+        elif self._TemporalSummaryType == TemporalSummaryTypes.D_ANNUAL:
+            rasterFilename = self.TryGetAnnualFileForDate(RequiredDate)
+        elif (self._TemporalSummaryType == TemporalSummaryTypes.S_MONTHLY_SD or
+            self._TemporalSummaryType == TemporalSummaryTypes.S_MONTHLY_MEAN):
+            rasterFilename = self.TryGetSynopticMonthlyFileForDate()
+        elif (self._TemporalSummaryType == TemporalSummaryTypes.S_ANNUAL_MEAN or
+            self._TemporalSummaryType == TemporalSummaryTypes.STATIC):
+            rasterFilename = self.StaticFilename
+
+        if rasterFilename:
+            dataArr, dataGT, dataProj, dataNDV = ReadAOI_PixelLims(rasterFilename)
+            return dataArr
+        else:
+            assert False
+
+
+
+
+
+    def TryGetMonthlyFileForDate(self, RequiredDate):
+        UseMonth = RequiredDate - self._TemporalLagMonths * ONEMONTH
+        if self.DynamicMonthlyFiles.has_key(UseMonth):
+            return self.DynamicMonthlyFiles[UseMonth]
         else:
             return None
 
-    def TryGetSynopticMonthlyFileForDate(self, RequiredMonth):
+    def TryGetAnnualFileForDate(self, RequiredDate):
+        # no lag involved with dynamic annual variables
+        UseYear = RequiredDate.year
+        if self.DynamicAnnualFiles.has_key(UseYear):
+            return self.DynamicAnnualFiles[UseYear]
+        else:
+            return None
+
+    def TryGetSynopticMonthlyFileForDate(self, RequiredDate):
+        UseMonth = RequiredDate.month
+        if self.SynopticMonthlyFiles.has_key(UseMonth):
+            return self.SynopticMonthlyFiles[UseMonth]
+        else:
+            return None
+
+
 
 
