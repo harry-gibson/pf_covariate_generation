e6f60187d60cb550394c474c6d1f6503cd1f9cff
harry-gibson <harry.s.gibson@gmail.com>
2018-03-22 17:24:22 +0000

    Create type for the transforms

--- variable_generation/transforms.py
+++ variable_generation/transforms.py
@@ -1,33 +1,54 @@
 import numexpr as ne
 import numpy as np
-def t1_untransformed(dataArr):
-    return arr
-def t2_normalize(dataArr, meanArr, stdArr):
-    expr= "(dataArr - meanArr) / stdArr"
-    return ne.evaluate(expr)
-def t3_reciprocal(dataArr):
-    expr = "1.0 / dataArr"
-    return ne.evaluate(expr)
-def t4_log10(dataArr):
-    expr = "log10(dataArr)"
-    return ne.evaluate(expr)
-def t5_ln(dataArr):
-    expr = "log(dataArr)"
-    return ne.evaluate(expr)
-def t6_ihs(dataArr):
-    expr = "log(dataArr + sqrt(dataArr ** 2 + 1))"
-    return ne.evaluate(expr)
-def t7_square(dataArr):
-    expr = "dataArr ** 2"
-    return ne.evaluate(expr)
-def t8_sqrt(dataArr):
-    expr = "sqrt(dataArr)"
-    return ne.evaluate(dataArr)
-def t9_cubrt(dataArr):
-    expr = "dataArr ** (1.0/3)"
-    return ne.evaluate(dataArr)
-def t10_boxcox(dataArr):
-    return whatthefuck
-def t11_absnormal(dataArr, meanArr, stdArr):
-    expr = "(dataArr / meanArr) / stdArr"
-    return ne.evaluate(expr)
+from enum import Enum
+
+class TransformTypes(Enum):
+    T1 = "Untransformed"
+    T2 = "Normalize"
+    T3 = "Reciprocal"
+    T4 = "Log10"
+    T5 = "Ln"
+    T6 = "InverseHyperbolicSine"
+    T7 = "Square"
+    T8 = "SquareRoot"
+    T9 = "CubeRoot"
+    T10 = "BoxCox"
+    T11 = "AbsNormal"
+
+class TransformAndAdjust:
+    def __init__(self, transformType, adjustmentOffset = 0):
+        assert isinstance(transformType, TransformTypes)
+        self.TransformType = transformType
+        self.AdjustmentOffset = adjustmentOffset
+
+    def Apply(self, dataArr, meanArr = None, stdArr = None):
+        if (self.TransformType == TransformTypes.T2 or
+            self.TransformType == TransformTypes.T11):
+            assert (meanArr is not None) and (stdArr is not None)
+            # TODO more checking, that they're the same size etc
+        adjOffset = self.AdjustmentOffset
+        if self.TransformType == TransformTypes.T1:
+            expr = "dataArr + adjOffset"
+        elif self.TransformType == TransformTypes.T2:
+            expr = "((dataArr + adjOffset) - meanArr) / stdArr"
+        elif self.TransformType == TransformTypes.T3:
+            expr = "1.0 / (dataArr + adjOffset)"
+        elif self.TransformType == TransformTypes.T4:
+            expr = "log10(dataArr + adjOffset)"
+        elif self.TransformType == TransformTypes.T5:
+            expr = "log(dataArr + adjOffset)"
+        elif self.TransformType == TransformTypes.T6:
+            expr = "log((dataArr + adjOffset) + sqrt((dataArr + adjOffset) ** 2 + 1))"
+        elif self.TransformType == TransformTypes.T7:
+            expr = "(dataArr + adjOffset) ** 2"
+        elif self.TransformType == TransformTypes.T8:
+            expr = "sqrt((dataArr + adjOffset))"
+        elif self.TransformType == TransformTypes.T9:
+            expr = "(dataArr + adjOffset) ** (1.0/3)"
+        elif self.TransformType == TransformTypes.T10:
+            return "whatthefuck" # TODO whatthefuck
+        elif self.TransformType == TransformTypes.T11:
+            expr = "((dataArr + adjOffset) - meanArr) / stdArr"
+        else:
+            raise ValueError()
+        return ne.evaluate(expr)
