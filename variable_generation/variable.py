b2de9dabb28503c1ddcbc8459715633e325c6b9c
harry-gibson <harry.s.gibson@gmail.com>
2018-04-09 16:03:48 +0100

    working implementation of MalariaCovariate as simple combination of two Terms

--- variable_generation/variable.py
+++ variable_generation/variable.py
@@ -1,26 +1,37 @@
 
-from term import FiveKCovariateVariableTerm
-
+from term import CovariateVariableTerm
+from transforms import  TransformTypes, TransformAndAdjust
 
 class MalariaCovariate:
 
-    def __init__(self, Term1, Transform1, Term2, Transform2):
-        assert isinstance(Term1, FiveKCovariateVariableTerm)
-        assert isinstance(Term2, FiveKCovariateVariableTerm)
+    def __init__(self, Term1, Term2):
+        assert isinstance(Term1, CovariateVariableTerm)
+        assert isinstance(Term2, CovariateVariableTerm)
         # todo define proper type for transforms, and check for it
         self.Term1 = Term1
         self.Term2 = Term2
-        self.Transform1 = Transform1
-        self.Transform2 = Transform2
 
-    def ProduceMonthData(self, RequiredMonth):
-        term1Data = self.Term1.ReadDataForDate(RequiredMonth)
-        # todo deal with transforms that need another array too
-        # todo do in one step to save mem
-        term1Data = self.Transform1(term1Data)
-        term2Data = self.Term2.ReadDataForDate(RequiredMonth)
-        term2Data = self.Transform2(RequiredMonth)
+
+    def ProduceMonthData(self, RequiredMonth, lonLims=None, latLims=None):
+        term1Data, _gt, _proj, _ndv = self.Term1.GenerateDataForDate(RequiredMonth,
+                                                                     lonLims=lonLims, latLims=latLims,
+                                                                     maskNoData=False)
+        term2Data, _gt2, _proj2, _ndv2 = self.Term2.GenerateDataForDate(RequiredMonth,
+                                                                        lonLims=lonLims, latLims=latLims,
+                                                                        maskNoData=False)
+
+        # todo check extent of the two variables and read them into either the union or intersection of the extents
+        # e.g. http://sciience.tumblr.com/post/101722591382/finding-the-georeferenced-intersection-between-two
 
         # all variables are the product of two transformed terms, i.e. just a straight multiplication of them
-        return term1Data * term2Data
+        ndMask = (term1Data == _ndv) + (term2Data == _ndv2)
+        res = term1Data * term2Data
+
+        res[ndMask] = _ndv
+        if ((_gt != _gt2) or (_proj != _proj2)):
+            Warning("geotransform or proj seem to be different")
+        return (res, _gt, _proj, _ndv)
+
+
+
 
